eaa0a1bac4117ff3ce002c592635a6bbf47c277a
angcyo <angcyo@126.com>
2020-12-22 18:10:44 +0800

    init

--- src/main/java/com/angcyo/main/Controller.kt
+++ src/main/java/com/angcyo/main/Controller.kt
@@ -1,9 +1,29 @@
 package com.angcyo.main
 
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.Button
+import java.net.URL
+import java.util.*
+
 /**
+ * [@FXML] 注解可以省略, 为了标识. 还是保留
  * Email:angcyo@126.com
  * @author angcyo
  * @date 2020/12/22
  */
-class Controller {
+class Controller : Initializable {
+
+    @FXML
+    lateinit var button: Button
+
+    @FXML
+    fun handleButtonClick() {
+        button.text = "test"
+    }
+
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+        println("initialize...$location $resources")
+    }
+
 }
