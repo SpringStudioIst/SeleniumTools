e78ac77ca11e88c7c1efeeb0a28f165a3c9159d7
angcyo <angcyo@126.com>
2021-01-08 22:17:01 +0800

    * fix name task bean

--- Tools/src/main/java/com/angcyo/javafx/bean/NameTaskBean.kt
+++ Tools/src/main/java/com/angcyo/javafx/bean/NameTaskBean.kt
@@ -1,5 +1,8 @@
 package com.angcyo.javafx.bean
 
+import com.angcyo.library.ex.nowTime
+import com.angcyo.library.file.uuid
+
 /**
  * Email:angcyo@126.com
  * @author angcyo
@@ -14,4 +17,24 @@ data class NameTaskBean(
     var nzCompanyTypeName: String? = null,//中文显示
     var companyWord: String? = null,
     var termsWord: String? = null,
-)
+
+    /**唯一标识*/
+    var uuid: String = uuid(),
+
+    /**创建时间*/
+    var createTime: Long = nowTime(),
+
+    /**任务状态*/
+    var taskState: Int = STATE_NORMAL,
+) {
+    companion object {
+        //正常
+        const val STATE_NORMAL = -1
+
+        //运行中
+        const val STATE_RUNNING = 1
+
+        //完成
+        const val STATE_FINISH = 0
+    }
+}
--- Tools/src/main/java/com/angcyo/javafx/item/DslNameTaskItem.kt
+++ Tools/src/main/java/com/angcyo/javafx/item/DslNameTaskItem.kt
@@ -3,13 +3,13 @@ package com.angcyo.javafx.item
 import com.angcyo.javafx.bean.NameTaskBean
 import com.angcyo.javafx.list.DslListItem
 import com.angcyo.javafx.list.DslListItemCell
-import com.angcyo.javafx.ui.gridPane
-import com.angcyo.javafx.ui.label
-import com.angcyo.javafx.ui.resetChildren
-import com.angcyo.javafx.ui.setGridIndex
+import com.angcyo.javafx.list.itemIndex
+import com.angcyo.javafx.ui.*
+import com.angcyo.library.ex.toTime
 import javafx.geometry.Insets
 import javafx.scene.Node
 import javafx.scene.layout.GridPane
+import javafx.scene.paint.Color
 
 /**
  * Email:angcyo@126.com
@@ -19,11 +19,11 @@ import javafx.scene.layout.GridPane
 class DslNameTaskItem : DslListItem() {
 
     var nameTaskBean: NameTaskBean? = null
+    val paddingValue = 5.0
 
     override fun onCreateItem(itemCell: DslListItemCell): Node? {
-        return itemCell.graphic ?: gridPane(1, 5) {
-            val value = 5.0
-            padding = Insets(value, value, value, value)
+        return itemCell.graphic ?: gridPane(2, 6) {
+            padding = Insets(paddingValue, paddingValue, paddingValue, paddingValue)
         }
     }
 
@@ -32,25 +32,87 @@ class DslNameTaskItem : DslListItem() {
         (rootNode as? GridPane)?.apply {
             //background = background(Color.AQUA)
             resetChildren(listOf(
+                //row 0
                 label {
-                    text = "账号:\n${nameTaskBean?.username}"
-                    setGridIndex(0, 0)
+                    setGridConstraints(0, 0, 1, 3)
+                    padding = Insets(paddingValue, paddingValue, paddingValue, 0.0)
+                    text = "任务:${itemIndex() + 1}  时间:${nameTaskBean?.createTime?.toTime() ?: ""}"
+                    textFill = Color.DEEPSKYBLUE
                 },
-                label {
-                    text = "密码:\n${nameTaskBean?.password}"
-                    setGridIndex(0, 1)
+                /*button {
+                    text = "开始"
+                    setGridIndex(0, 5)
+                    setOnAction {
+                        L.i("this...")
+                    }
+                },*/
+
+                //row 1
+                textFlow {
+                    setGridIndex(1, 0)
+                    //text = "账号:\n${nameTaskBean?.username}"
+                    resetChildren(
+                        listOf(
+                            text("帐号:\n", "#666666".toColor()),
+                            text(nameTaskBean?.username ?: "")
+                        )
+                    )
                 },
-                label {
-                    text = "企业类型:\n${nameTaskBean?.companyTypeName} ${nameTaskBean?.nzCompanyTypeName ?: ""}"
-                    setGridIndex(0, 2)
+                textFlow {
+                    setGridIndex(1, 1)
+                    //text = "密码:\n${nameTaskBean?.password}"
+                    resetChildren(
+                        listOf(
+                            text("密码:\n", "#666666".toColor()),
+                            text(nameTaskBean?.password ?: "")
+                        )
+                    )
                 },
-                label {
-                    text = "字号:\n${nameTaskBean?.companyWord}"
-                    setGridIndex(0, 3)
+                textFlow {
+                    setGridIndex(1, 2)
+                    //text = "企业类型:\n${nameTaskBean?.companyTypeName} ${nameTaskBean?.nzCompanyTypeName ?: ""}"
+                    resetChildren(
+                        listOf(
+                            text("企业类型:\n", "#666666".toColor()),
+                            text("${nameTaskBean?.companyTypeName} ${nameTaskBean?.nzCompanyTypeName ?: ""}")
+                        )
+                    )
                 },
-                label {
-                    text = "行业用语:\n${nameTaskBean?.termsWord}"
-                    setGridIndex(0, 4)
+                textFlow {
+                    setGridIndex(1, 3)
+                    //text = "字号:\n${nameTaskBean?.companyWord}"
+                    resetChildren(
+                        listOf(
+                            text("字号:\n", "#666666".toColor()),
+                            text(nameTaskBean?.companyWord ?: "")
+                        )
+                    )
+                },
+                textFlow {
+                    setGridIndex(1, 4)
+                    //text = "行业用语:\n${nameTaskBean?.termsWord}"
+                    resetChildren(
+                        listOf(
+                            text("行业用语:\n", "#666666".toColor()),
+                            text(nameTaskBean?.termsWord ?: "")
+                        )
+                    )
+                },
+                textFlow {
+                    setGridIndex(1, 5)
+                    resetChildren(
+                        listOf(
+                            text("任务状态:\n", Color.DEEPSKYBLUE),
+                            text(
+                                when (nameTaskBean?.taskState) {
+                                    NameTaskBean.STATE_NORMAL -> "就绪"
+                                    NameTaskBean.STATE_RUNNING -> "运行中"
+                                    NameTaskBean.STATE_FINISH -> "已完成"
+                                    else -> "--"
+                                }
+                            )
+                        )
+                    )
                 }
             ))
         }
