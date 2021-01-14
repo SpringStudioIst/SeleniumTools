e89481256a4491d63c6f66c6277934b117fe39fd
angcyo <angcyo@126.com>
2021-01-14 15:35:29 +0800

    + test

--- Tools/src/test/java/ExampleUnitTest.kt
+++ Tools/src/test/java/ExampleUnitTest.kt
@@ -16,7 +16,7 @@ class ExampleUnitTest {
     /**测试正则表达式*/
     @Test
     fun testRegex() {
-        val text = "l:0.1 t:100 w:-20.1width:>=99width:abc99 h:300"
+        val text = "l:>0.1 t:100 w:-20.1width:>=99width:abc99 h:300"
         val regex = "(?<=w:|width:)(?:[><=]*)([-]?[\\d.]*\\d+)".toRegex()
 //        val regex = "(?<=w:|width:)([><=]*)(?=[-]?[\\d.]*\\d+)".toRegex()
         //val regex = "([><=]*)(?=\\d+)".toRegex()
@@ -40,7 +40,7 @@ class ExampleUnitTest {
 
         val valueParse = ValueParse()
         //println(valueParse.parseExpression(text, "w:|width:", 100f))
-        println(valueParse.parseExpression(text, "l:", 100f))
+        println(valueParse.parseExpression(text, "", 1f))
     }
 
     /**测试正则表达式匹配*/
@@ -82,5 +82,42 @@ class ExampleUnitTest {
         println(text.encode())
         println("a:last+line b%3Alast+line".decode())
         println(" a:last+line b%3Alast+line ".count(' '))
+    }
+
+    @Test
+    fun testExpression() {
+        //val text = "l:>0.1 t:100 w:-20.1 h:%3.30<=2 t:1x2"
+        val text = "l:>0.1 t:100 w:-20.1 h:>3.30=2 t:1x2"
+
+        //第一步: 获取关键文本
+        val key = ":"
+        val r1 = "(?<=$key)(\\S+)".toRegex()
+        println(text.patternList(r1.toPattern()))
+
+        text.patternList(r1.toPattern()).forEach { keyText ->
+            //第二步: 获取判断表达式 >=4 %4
+            val r2 = "([><=%]*[-]?[\\d.]*\\d+)".toRegex()
+            println(keyText.patternList(r2.toPattern()))
+
+            keyText.patternList(r2.toPattern()).forEach { expText ->
+                print("${expText} 解析-> ")
+
+                //第三步: 获取比较符
+                val r3 = "([><=%]*)".toRegex()
+                print(expText.patternList(r3.toPattern()))
+
+                //第四步: 获取比较值
+                val r4 = "([-]?[\\d.]*\\d+)".toRegex()
+                print(expText.patternList(r4.toPattern()))
+
+                println()
+            }
+        }
+
+
+        /*val left = "(?<=h:)[><=%]*([-]?[\\d.]*\\d+)".toRegex()
+        val exp = "(?<=h:)([><=%]*)(?=[-]?[\\d.]*\\d+)".toRegex()
+        println(text.patternList(left.toPattern()))
+        println(text.patternList(exp.toPattern()))*/
     }
 }
